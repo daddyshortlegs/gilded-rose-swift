bf4457fea6e444b6f34fcb9738ba3feb91b827c5
andys <andysmith836@gmail.com>
2019-03-03 20:51:43 +0000

    Got rid of body from some ifs

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -35,25 +35,24 @@ public class GildedRose {
                         item.quality+=1
                     }
                 }
+                item.sellIn-=1
+                
+                if (item.sellIn < 0) {
+                    item.quality = 0
+                }
 
             } else if (item.name != sulfuras) {
+            } else if item.name == sulfuras {
+                
+            } else {
+                
+            }
+            
+            if item.name != agedBrie && item.name != passes && item.name != sulfuras {
                 if (item.quality > 0) {
                     item.quality-=1
                 }
-            }
-            
-            if item.name == agedBrie {
-            } else if (item.name == passes) {
-                item.sellIn-=1
 
-                if (item.sellIn < 0) {
-                    item.quality = 0
-                }
-            } else if (item.name == sulfuras) {
-                if (item.sellIn < 0) {
-                    item.quality = 0
-                }
-            } else {
                 item.sellIn-=1
 
                 if (item.sellIn < 0 && item.quality > 0) {
