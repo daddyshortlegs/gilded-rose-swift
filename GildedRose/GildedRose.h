913816b3a977352fa726964777a3be5a7071698c
andys <andy.smith@mkodo.com>
2019-03-03 14:14:17 +0000

    Initial Commit

---
+++ GildedRose/GildedRose.h
@@ -0,0 +1,19 @@
+//
+//  GildedRose.h
+//  GildedRose
+//
+//  Created by Andy Smith on 03/03/2019.
+//  Copyright © 2019 Andy Smith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GildedRose.
+FOUNDATION_EXPORT double GildedRoseVersionNumber;
+
+//! Project version string for GildedRose.
+FOUNDATION_EXPORT const unsigned char GildedRoseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GildedRose/PublicHeader.h>
+
+
