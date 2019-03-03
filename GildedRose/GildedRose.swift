8f22729a3851abad45a7c89c497b4016cbe70e8e
andys <andysmith836@gmail.com>
2019-03-03 15:10:52 +0000

    Extract or

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -12,27 +12,26 @@ public class GildedRose {
     public func updateQuality() {
         
         for item in items {
-            if (item.name == agedBrie || item.name == passes){
+            if (item.name == agedBrie) {
+                if (item.quality < 50) {
+                    item.quality+=1
+                }
+            } else if (item.name == passes) {
                 if (item.quality < 50) {
                     item.quality+=1
                     
-                    if (item.name == passes) {
-                        
-                        if (item.sellIn < 11 && item.quality < 50) {
-                            item.quality+=1
-                        }
-                        
-                        if (item.sellIn < 6 && item.quality < 50) {
-                            item.quality+=1
-                        }
-                        
+                    if (item.sellIn < 11 && item.quality < 50) {
+                        item.quality+=1
                     }
+                    
+                    if (item.sellIn < 6 && item.quality < 50) {
+                        item.quality+=1
+                    }                        
                 }
-            } else {
-                if (item.name != sulfuras) {
-                    if (item.quality > 0) {
-                        item.quality-=1
-                    }
+
+            } else if (item.name != sulfuras) {
+                if (item.quality > 0) {
+                    item.quality-=1
                 }
             }
             
