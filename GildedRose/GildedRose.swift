f8fffeb733e8f80fbfb8c36c00ce36d0b134eb8c
andys <andysmith836@gmail.com>
2019-03-03 15:08:06 +0000

    Negate another if

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -12,18 +12,11 @@ public class GildedRose {
     public func updateQuality() {
         
         for item in items {
-            if (item.name != agedBrie && item.name != passes) {
-                if (item.name != sulfuras) {
-                    if (item.quality > 0) {
-                        item.quality-=1
-                    }
-                }
-            } else {
+            if (item.name == agedBrie || item.name == passes){
                 if (item.quality < 50) {
                     item.quality+=1
                     
                     if (item.name == passes) {
-                        
                         
                         if (item.sellIn < 11 && item.quality < 50) {
                             item.quality+=1
@@ -32,7 +25,13 @@ public class GildedRose {
                         if (item.sellIn < 6 && item.quality < 50) {
                             item.quality+=1
                         }
-    
+                        
+                    }
+                }
+            } else {
+                if (item.name != sulfuras) {
+                    if (item.quality > 0) {
+                        item.quality-=1
                     }
                 }
             }
@@ -53,7 +52,6 @@ public class GildedRose {
                 if (item.sellIn < 0) {
                     item.quality = 0
                 }
-
             } else {
                 item.sellIn-=1
 
