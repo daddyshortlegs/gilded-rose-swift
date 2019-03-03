8b1ca5eb3d1d7d9504b9e9aaa44d0c116c0d5ed1
andys <andysmith836@gmail.com>
2019-03-03 20:56:51 +0000

    Extract 2nd function

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -10,29 +10,13 @@ public class GildedRose {
     }
     
     
+    
     public func updateQuality() {
         for item in items {
             if item.name == agedBrie {
                 agedBrieUpdate(item)
-
             } else if item.name == passes {
-                if (item.quality < 50) {
-                    item.quality+=1
-                    
-                    if item.sellIn < 11 && item.quality < 50 {
-                        item.quality+=1
-                    }
-                    
-                    if item.sellIn < 6 && item.quality < 50 {
-                        item.quality+=1
-                    }
-                }
-                item.sellIn-=1
-                
-                if item.sellIn < 0 {
-                    item.quality = 0
-                }
-
+                backstagePassesUpdate(item)
             } else if item.name == sulfuras {
                 
             } else {
@@ -60,5 +44,25 @@ public class GildedRose {
             item.quality+=1
         }
     }
+    
+    fileprivate func backstagePassesUpdate(_ item: Item) {
+        if (item.quality < 50) {
+            item.quality+=1
+            
+            if item.sellIn < 11 && item.quality < 50 {
+                item.quality+=1
+            }
+            
+            if item.sellIn < 6 && item.quality < 50 {
+                item.quality+=1
+            }
+        }
+        item.sellIn-=1
+        
+        if item.sellIn < 0 {
+            item.quality = 0
+        }
+    }
+
 
 }
