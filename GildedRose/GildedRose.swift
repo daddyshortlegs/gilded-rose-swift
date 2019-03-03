3a1e1e479a1a4fcce2af146687958d76442843b2
andys <andysmith836@gmail.com>
2019-03-03 20:55:11 +0000

    Final simplification of conditionals

--- GildedRose/GildedRose.swift
+++ GildedRose/GildedRose.swift
@@ -12,50 +12,45 @@ public class GildedRose {
     public func updateQuality() {
         
         for item in items {
-            if (item.name == agedBrie) {
-                if (item.quality < 50) {
+            if item.name == agedBrie {
+                if item.quality < 50 {
                     item.quality+=1
                 }
                 
                 item.sellIn-=1
                 
-                if (item.sellIn < 0 && item.quality < 50) {
+                if item.sellIn < 0 && item.quality < 50 {
                     item.quality+=1
                 }
 
-            } else if (item.name == passes) {
+            } else if item.name == passes {
                 if (item.quality < 50) {
                     item.quality+=1
                     
-                    if (item.sellIn < 11 && item.quality < 50) {
+                    if item.sellIn < 11 && item.quality < 50 {
                         item.quality+=1
                     }
                     
-                    if (item.sellIn < 6 && item.quality < 50) {
+                    if item.sellIn < 6 && item.quality < 50 {
                         item.quality+=1
                     }
                 }
                 item.sellIn-=1
                 
-                if (item.sellIn < 0) {
+                if item.sellIn < 0 {
                     item.quality = 0
                 }
 
-            } else if (item.name != sulfuras) {
             } else if item.name == sulfuras {
                 
             } else {
-                
-            }
-            
-            if item.name != agedBrie && item.name != passes && item.name != sulfuras {
-                if (item.quality > 0) {
+                if item.quality > 0 {
                     item.quality-=1
                 }
 
                 item.sellIn-=1
 
-                if (item.sellIn < 0 && item.quality > 0) {
+                if item.sellIn < 0 && item.quality > 0 {
                     item.quality-=1
                 }
             }
